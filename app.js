869c4d116d20c0ee607f867ef5f4caa10711a4c1
yusuf khan <patanyusufkhan222@gmail.com>
2021-03-08 12:58:25 +0530

    second

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const app = express();
 
 const userRoutes = require('./API/Routes/users.routes')
 
+app.use((req,res,next)=> {
+res.header('Access-Control-Allow-Origin','*');
+res.header('Access-Control-Allow-Headers','*');
+
+if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods','PUT,PATCH,POST,GET,DELETE');
+    return res.status(200).json({});
+}
+});
 
 app.use('/users',userRoutes);
 app.use((req,res,next)=>{
